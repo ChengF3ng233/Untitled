7366cb406fbf508d3a75a34e192d2774e76a0756
Feng Cheng <ChengF3ng233@outlook.com>
2024-08-06 17:47:35 +0800

    修复了一个错误

--- src/java/cn/feng/untitled/ui/MainMenu.java
+++ src/java/cn/feng/untitled/ui/MainMenu.java
@@ -1,8 +1,11 @@
 package cn.feng.untitled.ui;
 
+import net.minecraft.client.gui.GuiScreen;
+
 /**
  * @author ChengFeng
  * @since 2024/7/28
  **/
-public class MainMenu {
+public class MainMenu extends GuiScreen {
+
 }
