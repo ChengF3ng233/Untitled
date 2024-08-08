d79a809179bdeac31e23a43f8cd450563e18bbcd
Feng Cheng <ChengF3ng233@outlook.com>
2024-08-08 18:34:37 +0800

    不会写，删了

--- src/java/cn/feng/untitled/module/impl/render/Camera.java
+++ src/java/cn/feng/untitled/module/impl/render/Camera.java
@@ -1,10 +1,7 @@
 package cn.feng.untitled.module.impl.render;
 
-import cn.feng.untitled.event.api.SubscribeEvent;
-import cn.feng.untitled.event.impl.UpdateEvent;
 import cn.feng.untitled.module.Module;
 import cn.feng.untitled.module.ModuleCategory;
-import cn.feng.untitled.util.misc.ChatUtil;
 import cn.feng.untitled.value.impl.BoolValue;
 import cn.feng.untitled.value.impl.NumberValue;
 
@@ -13,24 +10,16 @@ import cn.feng.untitled.value.impl.NumberValue;
  * @since 2024/8/8
  **/
 public class Camera extends Module {
+    public static final BoolValue clip = new BoolValue("CameraClip", false);
+    public static final BoolValue animation = new BoolValue("CameraAnimation", true);
+    public static final BoolValue transform = new BoolValue("Transform", false);
+    public static final NumberValue x = new NumberValue("TransformX", 0f, 5f, 0f, 0.1f);
+    public static final NumberValue y = new NumberValue("TransformY", 0f, 5f, 0f, 0.1f);
+    public static final NumberValue z = new NumberValue("TransformZ", 0f, 5f, 0f, 0.1f);
     public Camera() {
         super("Camera", ModuleCategory.Render);
         if (!enabled)
             toggle();
         locked = true;
-    }
-
-    public static final BoolValue clip = new BoolValue("CameraClip", false);
-    public static final BoolValue animation = new BoolValue("CameraAnimation", true);
-    public static final BoolValue motion = new BoolValue("MotionCamera", false);
-
-    public static final BoolValue transform = new BoolValue("Transform", false);
-    public static final NumberValue x = new NumberValue("TransformX", 0f, 5f, 0f, 0.1f);
-    public static final NumberValue y = new NumberValue("TransformY", 0f, 5f, 0f, 0.1f);
-    public static final NumberValue z = new NumberValue("TransformZ", 0f, 5f, 0f, 0.1f);
-
-    @SubscribeEvent
-    private void onUpdate(UpdateEvent event) {
-
     }
 }
