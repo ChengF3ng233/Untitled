b60c9d5db1af6bc69ee515b0eb3bbb723abfc78e
Feng Cheng <ChengF3ng233@outlook.com>
2024-08-22 17:52:05 +0800

    perf: Particles

--- src/main/java/cn/feng/untitled/util/render/particle/Particle.java
+++ src/main/java/cn/feng/untitled/util/render/particle/Particle.java
@@ -19,21 +19,25 @@ public class Particle extends MinecraftInstance {
     private float y;
     private final float speed;
 
-    public Particle(float startX, float radius) {
-        this.radius = radius;
-        x = startX;
-        y = -radius;
+    public Particle() {
+        ScaledResolution sr = new ScaledResolution(mc);
         Random random = new Random();
+        radius = random.nextFloat(2.5f, 3f);
+        x = random.nextFloat(0, (float) sr.getScaledWidth_double());
+        y = -radius;
         speed = random.nextFloat(0.1f, 0.5f);
     }
 
     public void render() {
         ScaledResolution sr = new ScaledResolution(mc);
-        RoundedUtil.drawRound(x, y, radius, radius, radius / 3.5f, ColorUtil.applyOpacity(Color.WHITE, 0.6f));
+        RoundedUtil.drawRound(x, y, radius, radius, radius / 3.5f, ColorUtil.applyOpacity(Color.WHITE, 0.8f));
         x += (float) (speed * RenderUtil.frameTime) * 0.7f;
         y += (float) (speed * RenderUtil.frameTime);
 
-        if (y > sr.getScaledHeight()) y = -radius;
+        if (y > sr.getScaledHeight()) {
+            y = -radius;
+        }
+
         if (x > sr.getScaledWidth()) {
             x = 0;
         }
--- src/main/java/cn/feng/untitled/util/render/particle/ParticleManager.java
+++ src/main/java/cn/feng/untitled/util/render/particle/ParticleManager.java
@@ -2,8 +2,6 @@ package cn.feng.untitled.util.render.particle;
 
 import cn.feng.untitled.util.MinecraftInstance;
 import cn.feng.untitled.util.misc.TimerUtil;
-import lombok.Setter;
-import net.minecraft.client.gui.ScaledResolution;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,19 +15,17 @@ public class ParticleManager extends MinecraftInstance {
     private final List<Particle> particleList = new ArrayList<>();
     private final Random random = new Random();
     private final TimerUtil timer = new TimerUtil();
-    @Setter
-    private int amount = 0;
 
-    public void renderParticles() {
-        if (particleList.size() <= amount && timer.hasTimeElapsed(random.nextLong(1000, 3000))) {
-            ScaledResolution sr = new ScaledResolution(mc);
-            float startX = random.nextFloat(0, (float) sr.getScaledWidth_double());
-            particleList.add(new Particle(startX, random.nextFloat(2.5f, 3f)));
+    public void renderParticles(long minAppearInterval, long maxAppearInterval, int amount) {
+        if (particleList.size() <= amount && timer.hasTimeElapsed(random.nextLong(minAppearInterval, maxAppearInterval))) {
+            particleList.add(new Particle());
             timer.reset();
         }
+
         if (particleList.size() > amount) {
             particleList.remove(particleList.size() - 1);
         }
+
         particleList.forEach(Particle::render);
     }
 }
